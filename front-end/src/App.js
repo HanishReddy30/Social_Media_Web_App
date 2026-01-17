aca834db540f24ccaceb459005b3962c6b81d5a5
HanishReddy30 <homemamidi5@gmail.com>
2026-01-17 14:53:10 +0530

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -26,6 +26,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile/:id" element={<Profile />} />
+          {/* updates takes long time */}
         </Routes>
       </div>
     </Router>
